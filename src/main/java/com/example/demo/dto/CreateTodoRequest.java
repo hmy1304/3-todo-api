7fd226d8c648ac781a290ae825c1eaa2012d8ee5
unknown <hmy1304@kbu.ac.kr>
2026-03-11 19:46:53 +0900

    update

---
+++ src/main/java/com/example/demo/dto/CreateTodoRequest.java
@@ -0,0 +1,11 @@
+package com.example.demo.dto;
+
+import lombok.Getter;
+
+@Getter
+
+public class CreateTodoRequest {
+    private String title;
+    private String content;
+
+}
---
+++ src/main/java/com/example/demo/dto/UpdateTodoRequest.java
@@ -0,0 +1,11 @@
+package com.example.demo.dto;
+
+import lombok.Getter;
+
+@Getter
+
+public class UpdateTodoRequest {
+    private String title;
+    private String content;
+    private boolean completed;
+}
