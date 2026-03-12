ef05a54624c859b17e3332355b84e6159cd02879
unknown <hmy1304@kbu.ac.kr>
2026-03-12 16:22:31 +0900

    update

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.CreateTodoRequest;
 import com.example.demo.dto.TodoResponse;
+import com.example.demo.dto.UpdateTodoRequest;
 import com.example.demo.service.TodoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -22,5 +23,23 @@ public class TodoController {
     @GetMapping
     public List<TodoResponse> findAll() {
         return todoService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public TodoResponse findById(@PathVariable Long id) {
+        return todoService.findById(id);
+    }
+
+    @PatchMapping("/{id}")
+    public TodoResponse update(
+            @PathVariable Long id,
+            @RequestBody UpdateTodoRequest request
+            ) {
+        return todoService.update(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        todoService.delete(id);
     }
 }
